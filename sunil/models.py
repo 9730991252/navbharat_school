7d1974071f9674f27bb7b9a4202e22dd65802125
9730991252 <sunilkale101@gmail.com>
2025-04-06 17:31:29 +0530

    ok

--- sunil/models.py
+++ sunil/models.py
@@ -1,5 +1,5 @@
 from django.db import models
 
 # Create your models here.
-class sunil(models.Model):
-    sum = models.IntegerField()
+# class sunil(models.Model):
+#     sum = models.IntegerField()
