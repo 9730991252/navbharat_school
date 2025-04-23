5ac0888063744818d1d7024fed04d940d455e23e
9730991252 <sunilkale101@gmail.com>
2025-04-23 09:13:12 +0530

    ok

--- school_admin/urls.py
+++ school_admin/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('admin_home/', views.admin_home, name='admin_home'),
+    path('admin_view_students/', views.admin_view_students, name='admin_view_students'),
+    path('admin_view_teacher/', views.admin_view_teacher, name='admin_view_teacher'),
 ]
--- school_admin/views.py
+++ school_admin/views.py
@@ -15,3 +15,44 @@ def admin_home(request):
         return render(request, 'admin_home.html', context)
     else:
         return redirect('admin_login')
+    
+def admin_view_students(request):
+    if request.session.has_key('admin_mobile'):
+        mobile = request.session['admin_mobile']
+        s = []
+        for i in Student.objects.all():
+            s.append({
+                'id':i.id,
+                'name':i.name,
+                'mobile':i.mobile,
+                'aadhar_number':i.aadhar_number,
+                'gender':i.gender,
+                'img':Student_Image.objects.filter(student_id=i.id).first(),
+                'class':Class_student.objects.filter(student_id=i.id).first(),
+            })
+        context={
+            'students':s,
+            'total_students':Student.objects.all().count(),
+            'male_students':Student.objects.filter(gender='MALE').count(),
+            'female_students':Student.objects.filter(gender='FEMALE').count(),
+        }
+        return render(request, 'admin_view_students.html', context)
+    else:
+        return redirect('admin_login')
+    
+def admin_view_teacher(request):
+    if request.session.has_key('admin_mobile'):
+        mobile = request.session['admin_mobile']
+        s = []
+        for i in Teacher.objects.all():
+            s.append({
+                'id':i.id,
+                'name':i.name,
+                'mobile':i.mobile,
+            })
+        context={
+            'teachers':s,
+        }
+        return render(request, 'admin_view_teacher.html', context)
+    else:
+        return redirect('admin_login')
